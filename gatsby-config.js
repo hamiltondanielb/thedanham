a825d4163c80191d6b7c03ff7586a50f028588e0
Dan <hamiltondanielb@users.noreply.github.com>
2020-09-15 22:02:10 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Dan Hamilton`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Dan Hamilton`,
+      summary: `who lives and works in Austin building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `A starter blog demonstrating what Dan can do.`,
+    siteUrl: `https://thedanham.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `thedanhamilton`,
     },
   },
   plugins: [
